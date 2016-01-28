84e3eeee9e22191d3e96fde4c05058854ce66b85
An Janet <janetregala23@gmail.com>
2016-01-28 15:22:45 +0800

    update

---
+++ src/com/pojo/test/POJO.java
@@ -0,0 +1,20 @@
+package com.pojo.test;
+
+public class POJO {
+
+	String song;
+	String singer;
+	
+	public String getSong() {
+		return song;
+	}
+	public void setSong(String song) {
+		this.song = song;
+	}
+	public String getSinger() {
+		return singer;
+	}
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+}
--- src/com/springmvc/controller/SpringmvcHelloWorld.java
+++ src/com/springmvc/controller/SpringmvcHelloWorld.java
@@ -4,18 +4,33 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import com.pojo.test.POJO;
 
 @Path("/welcome")
 public class SpringmvcHelloWorld {
 
-	@GET
+	/*@GET
 	@Path("/{parameter}")
 	public Response responseMsg( @PathParam("parameter") String parameter,
 			@DefaultValue("Nothing to say") @QueryParam("value") String value) {
 
 		String output = "Hello from : " + parameter + " : " + value;
 		return Response.status(200).entity(output).build();
+	}*/
+	
+	@GET
+	@Path("/pojo")
+	@Produces(MediaType.APPLICATION_JSON)
+	public POJO showMusic(){		
+		POJO music = new POJO();
+		music.setSong("Out of the woods");
+		music.setSinger("Taylor Swift");
+		return music;
+		
 	}
 }
